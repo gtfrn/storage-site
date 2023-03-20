0df6e108f4cabadde29bda7469f2c3f8415efca0
gtfrn <128431285+gtfrn@users.noreply.github.com>
2023-03-20 18:34:39 -0400

    Add files via upload

---
+++ landing/script.js
@@ -0,0 +1,11 @@
+document.getElementById("payBillBtn").onclick = function() {
+    // Add logic to navigate or perform action for "Pay bill"
+};
+
+document.getElementById("uHaulBtn").onclick = function() {
+    // Add logic to navigate or perform action for "U-Haul"
+};
+
+document.getElementById("learnMoreBtn").onclick = function() {
+    // Add logic to navigate or perform action for "Learn more..."
+};
